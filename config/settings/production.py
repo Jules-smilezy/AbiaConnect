9820bf656e2f404a587053fe0f946a9dba055b65
unknown <nwosuemmanuel159@gmail.com>
2025-05-15 17:03:47 +0100

    Commited

---
+++ config/settings/production.py
@@ -0,0 +1,58 @@
+from .base import *
+
+# SECURITY WARNING: don't run with debug turned on in production!
+DEBUG =  False
+
+ALLOWED_HOSTS = ['moldaton.com', '159.223.179.77', 'www.growingseeds.com']
+
+# Database
+# https://docs.djangoproject.com/en/3.2/ref/settings/#databases
+
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': 'social',
+        'USER': 'social',
+        'PASSWORD': 'def@ult2017',
+        'HOST': 'localhost',
+        'PORT': '',
+    }
+}
+
+
+
+STATIC_ROOT = BASE_DIR/ 'static'
+
+
+
+
+LOGGING = {
+    "version": 1,
+    "disable_existing_loggers": False,
+    "root": {"level": "INFO", "handlers": ["file"]},
+    "handlers": {
+        "file": {
+            "level": "INFO",
+            "class": "logging.FileHandler",
+            "filename": "/var/log/earlystart_log.log",
+            "formatter": "app",
+        },
+    },
+    "loggers": {
+        "django": {
+            "handlers": ["file"],
+            "level": "INFO",
+            "propagate": True
+        },
+    },
+    "formatters": {
+        "app": {
+            "format": (
+                u"%(asctime)s [%(levelname)-8s] "
+                "(%(module)s.%(funcName)s) %(message)s"
+            ),
+            "datefmt": "%Y-%m-%d %H:%M:%S",
+        },
+    },
+}
+
