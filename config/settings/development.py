741b343a66d2a1747770bac2a37c4c4c50f38157
Ogechi Meribe <meribeogechi1@gmail.com>
2025-05-17 11:27:03 -0700

    fixed

--- config/settings/development.py
+++ config/settings/development.py
@@ -3,7 +3,7 @@ from .base import *
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['https://abiaconnect.onrender.com', 'abiaconnect.onrender.com' ]
+ALLOWED_HOSTS = ['abiaconnect.onrender.com', 'abiaconnect.onrender.com', 'smilezy.pythonanywhere.com' ]
 
 # Database
 # https://docs.djangoproject.com/en/3.2/ref/settings/#databases
